ff57344ba94a1c4b2d919eaa6433c710b088c42f
Nyaina61 <nyainajoel61@gmail.com>
2023-12-05 08:41:34 +0300

    Author

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Entity\User;
+use App\Entity\Category;
+use App\Entity\ExtraData;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AuthorController extends AbstractController
+{
+ private ManagerRegistry $doctrine;
+
+ public function __construct(ManagerRegistry $doctrine)
+ {
+     $this->doctrine = $doctrine;
+ }
+
+ #[Route('/authors/{id}/extraData', methods: ['POST'])]
+ public function createCategoryForUser(int $id, Request $request, EntityManagerInterface $entityManager): Response
+ {
+     // Récupérer l'utilisateur
+     $author = $this->doctrine->getRepository(Author::class)->find($id);
+
+     if (!$author) {
+         throw $this->createNotFoundException('No user found for id '.$id);
+     }
+
+     // Récupérer les données de la requête
+     $data = json_decode($request->getContent(), true);
+
+     // Créer une nouvelle instance de Category
+     $extraData = new ExtraData();
+     $extraData->setTitle($data['title']);
+     $extraData->setContent($data['content']);
+
+     // Associer la catégorie à l'utilisateur
+     $author->addExtraData($extraData);
+
+     // Persister l'utilisateur et la nouvelle catégorie
+     $entityManager->persist($author);
+     $entityManager->flush();
+
+     // Retourner une réponse
+     return new Response('Category created successfully', Response::HTTP_CREATED);
+ }
+}
